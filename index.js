fbd1db8e391929893aa8e0cd6f0f051a870c99eb
Nick Gastis <nickgastis1@gmail.com>
2023-02-24 10:37:41 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer
+
+var upperCaseCustomerName = () => {
+ customerName = customerName.toUpperCase()
+}
+
+var setBestCustomer = () => {
+bestCustomer = 'not bob'
+}
+
+var overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob';
+  }
+
+  const leastFavoriteCustomer = 'dave'
+
+  var changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'Sam'
+  }
